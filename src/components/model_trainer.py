a2b10c17faa7d26537592306c226c87bb01146d9
priyansahay <priyanshusahay489@gmail.com>
2023-06-23 16:29:41 +0530

    Deployment Configuration

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -50,7 +50,7 @@ class ModelTrainer:
                 "AdaBoost Classifier": AdaBoostRegressor()
 
             }
-            model_report:dict=evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models,params=params)
+            model_report:dict=evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models)
             best_model_score = max(sorted(model_report.values()))
 
             best_model_name = list(model_report.keys())[
