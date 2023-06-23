7c895d8f359c6e6e42ef70cc74ae567a99b8b856
priyansahay <priyanshusahay489@gmail.com>
2023-06-23 12:50:57 +0530

    Deployment Configuration

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
-HYPHON_E_DOT= "-e ."
+
+HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        [req.replace("\n","")for req in requirements]
-        if HYPHON_E_DOT in requirements:
-            requirements.remove(HYPHON_E_DOT)
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
 setup (
     name='mlprojects',
     version='0.0.1',
--- src/utils.py
+++ src/utils.py
@@ -9,7 +9,7 @@ import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
-from exception import CustomException
+from src.exception import CustomException
 
 def save_object(file_path, obj):
     try:
@@ -29,15 +29,13 @@ def evaluate_models(x_train, y_train,x_test,y_test,models,param):
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            para=param[list(models.keys())[i]]
+            
 
-            gs = GridSearchCV(model,para,cv=3)
-            gs.fit(x_train,y_train)
-
-            model.set_params(**gs.best_params_)
+            
+            
             model.fit(x_train,y_train)
 
-            #model.fit(X_train, y_train)  # Train model
+            
 
             y_train_pred = model.predict(x_train)
 
@@ -57,7 +55,9 @@ def evaluate_models(x_train, y_train,x_test,y_test,models,param):
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
-            return pickle.load(file_obj)
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
+
+
